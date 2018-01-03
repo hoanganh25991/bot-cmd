83eda082420e1b7a6ebbdcd58eb92a45ad7b0e34
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-01-03 14:24:01 +0700

    Test cors

--- handler.js
+++ handler.js
@@ -1,41 +1,25 @@
 const sayHello = require("./src/api")
 
 module.exports.hello = (event, context, callback) => {
-  // const message = sayHello()
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message,
-  //     input: event,
-  //   }),
-  // };
-
+  const message = sayHello()
   const path = event.path
 
   const response = {
     statusCode: 200,
+    headers: {
+      // Required for CORS support to work
+      "Access-Control-Allow-Origin" : "*"
+    },
     body: JSON.stringify({
-      message: `hello ${path}`,
+      message: `hello ${path}: ${message}`,
       input: event,
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
 module.exports.hello2 = (event, context, callback) => {
-  // const message = sayHello()
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message,
-  //     input: event,
-  //   }),
-  // };
-
   const path = event.path
 
   const response = {
@@ -47,7 +31,4 @@ module.exports.hello2 = (event, context, callback) => {
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,9 @@ const getData = require("../mongo")
 const sayHello = () => {
   const coders = getData()
   const helloStr = coders.reduce((carry, coder) => {
-    carry += `Hi, ${coder.name}`
+    carry += `${coder.name},`
     return carry
-  }, "")
+  }, "Hi, ")
   return helloStr
 }
 
