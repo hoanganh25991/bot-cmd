253b7f95ef6af0e52bb9168b9997992ac2a3b666
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-01-03 14:09:35 +0700

    Multiple endpoints, multiple functions

--- handler.js
+++ handler.js
@@ -10,10 +10,38 @@ module.exports.hello = (event, context, callback) => {
   //   }),
   // };
 
+  const path = event.path
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "hello",
+      message: `hello ${path}`,
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.hello2 = (event, context, callback) => {
+  // const message = sayHello()
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message,
+  //     input: event,
+  //   }),
+  // };
+
+  const path = event.path
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `hello2 ${path}`,
       input: event,
     }),
   };
