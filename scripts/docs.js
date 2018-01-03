5b57db897893459fdf2c5e90cf3abd39e077d2d7
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-01-03 20:48:43 +0700

    Remove unused files

--- scripts/docs.js
+++
@@ -1,7 +0,0 @@
-import cpr from "child_process"
-
-const _ = console.log
-const cmd = `postmanerator -output=tmp/doc.html -collection=tmp/vnexcoin.postman_collection.json`
-
-_("[INFO] Generate docs in tmp folder")
-_(cpr.execSync(cmd).toString())
