775855d0a07a9f8a453bfb3dc27a18007ece3b59
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-01-03 21:36:06 +0700

    Layout cmd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+const program = require("commander")
+const { createFunc, integrate } = require("./lamda")
+
+program.version("0.0.1").description("Serverless with lamda")
+
+program
+  .command("function <name>")
+  .alias("func")
+  .description("Add a lamda function")
+  .action(name => {
+    createFunc(name)
+  })
+
+program
+  .command("integrate <plaform>")
+  .alias("i")
+  .description("Integrate lamda func to platform")
+  .action(name => integrate(name))
+
+program.parse(process.argv)
---
+++ src/lamda/index.js
@@ -0,0 +1,2 @@
+module.exports.createFunc = name => console.log(`Creating... ${name}`)
+module.exports.integrate = name => console.log(`Integrating... ${name}`)
